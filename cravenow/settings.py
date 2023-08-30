fe954b4ca52b8e3a5fc8bb49d94147b2453d9059
Mahboob-Alam <iammahboob.a@gmail.com>
2023-08-30 12:07:23 +0530

    customized .env and .env-sample

--- cravenow/settings.py
+++ cravenow/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config   # for .env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9i$v1_6$@@6frx&kjbfm!404s%(xpn-cvs3(wtvv-e##g2l+bm'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -77,10 +78,10 @@ WSGI_APPLICATION = 'cravenow.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CraveNow',
-        'USER' : 'postgres', 
-        'PASSWORD' : 'mypglearning123',
-        'HOST' : 'localhost',
+        'NAME': config('NAME'),
+        'USER' : config('USER'),
+        'PASSWORD' : config('PASSWORD'),
+        'HOST' : config('HOST')
     }
 }
 
